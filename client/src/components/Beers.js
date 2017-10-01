9553ae65143d741a2d79657f1ad2020b9c874ae1
Evan Radford <Eradford45@gmail.com>
2017-10-01 13:18:57 -0600

    Cardgroup shows up and says hello.. getting data from the server but no idea how to display it in its format

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -1,32 +1,34 @@
-import {React, Component} from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Card } from 'semantic-ui-react';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class Beers extends Component {
   state = { beers: []}   
-    componentDidMount() {
-    axios.get('/api/all_beers.json')
-      .then( res => {
-       this.setState({ beers: res.data });
-      })
-  }
-
-  allBeers = () => {
-    return this.state.beers.map(s => {
-      return( <Card
-      header="`${s.name}`"
-      description='`${s.description}`'
-      />
-        
-      )
+  componentDidMount() {
+    axios.get("/api/all_beers").then(res => {
+      this.setState({ beers: res.data });
     });
   }
   
+  
+  
   render () {
     const { beers } = this.setState;
-    return(
-     this.allBeers
+    return( 
+      <Card.Group>
+        <Card>
+          <Card.Content>  
+            <Card.Header>
+              "Hello World"
+            </Card.Header>
+            <Card.Description>
+              "Hellow World"
+            </Card.Description>
+          </Card.Content>
+        </Card>
+     </Card.Group>
     )
   }
 } 
